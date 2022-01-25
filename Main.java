0b6555d5aa902ae383b762c7c27b67e9aa22b986
2102524 <97451318+2102524@users.noreply.github.com>
2022-01-25 09:14:33 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package lab1ex3;
+
+public class Main {
+    public static void main(String args[]){
+        for(int x = 102; x >= 66; x--){
+            if (x%2 != 0){
+                System.out.println(x);
+            }
+        }
+    }
+}
